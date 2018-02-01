656c5ff2f3258b8f73eb3404517500acb2e6c4e3
cristiab <cristiab@netapp.com>
2018-02-01 14:46:43 +0200

    intermediary

--- ch9.py
+++ ch9.py
@@ -285,8 +285,73 @@ def countdown(n):
     while n > 0:
         n -= 1
 
+"""Defining a Decorator That Takes an Optional Argument"""
 "https://web.archive.org/web/20171130045115/http://chimera.labs.oreilly.com/books/1230000000393/ch09.html#_discussion_149"
+from functools import wraps, partial
+import logging
+def logged(func=None, *, level=logging.DEBUG, name=None, message=None):
+    if func is None:
+        return partial(logged, level=level, name=name, message=message)
+
+    logname = name if name else func.__module__
+    log = logging.getLogger(logname)
+    logmsg = message if message else func.__name__
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        log.log(level, logmsg)
+        return func(*args, **kwargs)
+    return wrapper
+
+# Example use
+@logged
+def add(x, y):
+    return x + y
+
+@logged(level=logging.CRITICAL, name='example')
+def spam():
+    print('Spam!')
+
+
 "https://web.archive.org/web/20171130045115/http://chimera.labs.oreilly.com/books/1230000000393/ch09.html#_problem_150"
+"""Enforcing Type Checking on a Function Using a Decorator"""
+
+from inspect import signature
+from functools import wraps
+
+def typeassert(*ty_args, **ty_kwargs):
+    def decorate(func):
+        # If in optimized mode, disable type checking
+        if not __debug__:
+            return func
+
+        # Map function argument names to supplied types
+        sig = signature(func)
+        bound_types = sig.bind_partial(*ty_args, **ty_kwargs).arguments
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            bound_values = sig.bind(*args, **kwargs)
+            # Enforce type assertions across supplied arguments
+            for name, value in bound_values.arguments.items():
+                if name in bound_types:
+                    if not isinstance(value, bound_types[name]):
+                      raise TypeError(
+                        'Argument {} must be {}'.format(name, bound_types[name])
+                        )
+            return func(*args, **kwargs)
+        return wrapper
+    return decorate
+
+
+@typeassert(int, int)
+def add(x, y):
+    return x + y
+
+@typeassert(int, z=int)
+def spam(x, y, z=42):
+    print(x, y, z)
+
+
 "https://web.archive.org/web/20171130045115/http://chimera.labs.oreilly.com/books/1230000000393/ch09.html#_problem_151"
 "https://web.archive.org/web/20171130045115/http://chimera.labs.oreilly.com/books/1230000000393/ch09.html#_problem_152"
 "https://web.archive.org/web/20171130045115/http://chimera.labs.oreilly.com/books/1230000000393/ch09.html#_problem_153"
