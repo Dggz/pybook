e58d98ee0495162d5052a009c3c3df61daf47fb5
cbaciu <cristianbaciu95@gmail.com>
2018-11-21 11:26:31 +0200

    added round to higher ten function, cleanup

--- cab.py
+++ cab.py
@@ -1,31 +1,28 @@
-"""
-Created on Dec 15, 2016
-
-@author: Dggz
-"""
 
 def file_print(msg, output_file):
     output_file.write(msg + '\n')
     print(msg)
 
 
+def round_ten(number):
+    rounded = round(number, -1)
+    return rounded if number < rounded else rounded + 10
+
+
 f = open('cab.txt', 'w')
 
-# pret_cabana = int(input("\nPret cabana: ")) # ex: 6000
-pret_cabana = 8500
+pret_cabana = 8500 # pret_cabana = int(input("\nPret cabana: ")) # ex: 6000
 s1 = float(pret_cabana / 3)
 f.write("\nPret cabana: {}".format(pret_cabana))
 file_print("\nPret pe seara: " + str(round(s1)), f)
 
 p1 = int(input("\nPersoane o seara: "))
-p2 = int(input("\nPersoane 2 seri: "))
-# p2 = p1
+p2 = int(input("\nPersoane 2 seri: ")) # p2 = p1
 p3 = int(input("\nPersoane 3 seri: "))
 
 f.write("\nPersoane seara 1: {}"
         "\nPersoane seara 2: {}"
         "\nPersoane seara 3: {}\n".format(p3 + p2, p3 + p2, p3 + p1))
-# print('---input done---')
 
 s1, s2, s3 = round(s1/(p3 + p2) + 1), round(s1/(p3 + p2)), round(s1/(p3 + p1))
 
@@ -34,36 +31,22 @@ tbl += "\nPret seara 2/pers: " + str(s2)
 tbl += "\nPret seara 3/pers: " + str(s3)
 # f.write(tbl)
 # file_print(tbl, f)
-# print('---intermediary done---')
-
 
 s2ind = s1 + s2
-# s3ind = s2ind + s3
 
+s2ind = round_ten(round(s2ind / 2 * 2.25))
 
-s2ind = int(round(s2ind / 2 * 2.25))
-rounded = round(s2ind, -1)
-s2ind = rounded if s2ind < rounded else rounded + 10
-# max_s = (p1 + p2 + p3) / 3
+s3ind = round_ten((pret_cabana - (p2 * s2ind + p1 * s1 )) / p3)
 
-c2s = p2
-
-s3ind = int((pret_cabana - (c2s * s2ind + p1 * s1 )) / p3)
-s3rounded = round(s3ind, -1)
-s3ind = s3rounded if s3ind < s3rounded else s3rounded + 10
-
-cap = 550 # s2ind + s1 * 1.5
+cap = 550
 
 if s3ind > cap:
     s3ind = cap
     rest = pret_cabana - p3 * s3ind - p1 * s1
     rounded = round(rest / p2, -1)
-    # import ipdb; ipdb.set_trace()
     s2ind = rounded if rest / p2 < rounded else rounded + 10
-    # s2ind = rest / (p2)
 
 
-# import ipdb; ipdb.set_trace()
 final = '\nPret pt o seara: {}\n'.format(s1)
 final += '\nPret pt 2 seri: {}\n'.format(s2ind)
 # final = '\nPret pt 2 seri: {}\n'.format(s2ind)
--- cabana.py
+++ cabana.py
@@ -1,31 +1,28 @@
-"""
-Created on Dec 15, 2016
-
-@author: Dggz
-"""
 
 def file_print(msg, output_file):
     output_file.write(msg + '\n')
     print(msg)
 
 
+def round_ten(number):
+    rounded = round(number, -1)
+    return rounded if number < rounded else rounded + 10
+
+
 f = open('cabana.txt', 'w')
 
-# pret_cabana = int(input("\nPret cabana: ")) # ex: 6000
-pret_cabana = 8500
+pret_cabana = 8500 # pret_cabana = int(input("\nPret cabana: ")) # ex: 6000
 s1 = float(pret_cabana / 3)
 f.write("\nPret cabana: {}".format(pret_cabana))
 file_print("\nPret pe seara: " + str(round(s1)), f)
 
 p1 = int(input("\nPersoane seara 1: "))
-p2 = int(input("\nPersoane seara 2: "))
-# p2 = p1
+p2 = int(input("\nPersoane seara 2: ")) # p2 = p1
 p3 = int(input("\nPersoane seara 3: "))
 
 f.write("\nPersoane seara 1: {}"
         "\nPersoane seara 2: {}"
         "\nPersoane seara 3: {}\n".format(p1, p2, p3))
-# print('---input done---')
 
 s1, s2, s3 = round(s1/p1), round(s1/p2), round(s1/p3)
 
@@ -34,16 +31,10 @@ tbl += "\nPret seara 2/pers: " + str(s2)
 tbl += "\nPret seara 3/pers: " + str(s3)
 # f.write(tbl)
 file_print(tbl, f)
-# print('---intermediary done---')
-
 
 s2ind = s1 + s2 + 2
-# s3ind = s2ind + s3
-
 
 s2ind = int(round(s2ind / 2 * 2.25)) + 2
-
-max_s = (p1 + p2) / 2
 
 c2s = p1 - p3
 
@@ -55,12 +46,9 @@ if s3ind > cap:
     s3ind = cap
     rest = pret_cabana - p3 * s3ind
     rounded = round(rest / (p2 - p3), -1)
-    # import ipdb; ipdb.set_trace()
     s2ind = rounded if rest / (p2 - p3) < rounded else rounded + 10
-    # s2ind = rest / (p2 - p3)
 
 
-# import ipdb; ipdb.set_trace()
 final = '\nPret pt o seara: {}\n'.format(s1)
 final += '\nPret pt 2 seri: {}\n'.format(s2ind)
 # final = '\nPret pt 2 seri: {}\n'.format(s2ind)
