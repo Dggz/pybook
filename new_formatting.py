0eee120af585bf7e25d87e8b69a520d75409f5e9
cbaciu <cristianbaciu95@gmail.com>
2018-11-27 12:32:30 +0200

    fstrings presentation

---
+++ new_formatting.py
@@ -0,0 +1,73 @@
+
+# Old % way
+print('\n%\n')
+
+age = 74
+name = "Eric"
+print("Hello, %s. You are %s." % (name, age))
+
+age = 74
+first_name = "Eric"
+last_name = "Idle"
+profession = "comedian"
+affiliation = "Monty Python"
+print("Hello, %s %s. You are %s. You are a %s. You were a member of %s." % (
+    first_name, last_name, age, profession, affiliation
+))
+
+
+# New-ish str.format()
+
+print('\nstr.format()\n')
+
+print("Hello, {}. You are {}.".format(name, age))
+
+print("Hello, {1}. You are {0}.".format(age, name))
+
+person = {'name': 'Eric', 'age': 74}
+print("Hello, {name}. You are {age}.".format(name=person['name'], age=person['age']))
+
+first_name = "Eric"
+last_name = "Idle"
+age = 74
+profession = "comedian"
+affiliation = "Monty Python"
+print("Hello, {first_name} {last_name}. You are {age}. You are a {profession}."
+      "You were a member of {affiliation}."
+      .format(first_name=first_name, last_name=last_name, age=age,
+              profession=profession, affiliation=affiliation))
+
+# f-strings
+
+print('\nf-strings\n')
+age = 74
+name = "Eric"
+print(f"Hello, {name}. You are {age}.")
+
+age = 74
+first_name = "Eric"
+last_name = "Idle"
+profession = "comedian"
+affiliation = "Monty Python"
+print(f"Hello, {first_name} {last_name}. You are {age}. You are a {profession}."
+      f" You were a member of {affiliation}.")
+
+print(f"{name.lower()} is funny.")
+
+print(f"2 * 37")
+
+class Comedian:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} is {self.age}."
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name} is {self.age}. Surprise!"
+
+
+new_comedian = Comedian("Eric", "Idle", "74")
+print(f"{new_comedian}")
---
+++ rp5ro.py
@@ -0,0 +1,10 @@
+
+mancare = int(input("Cantitate de mancare: "))
+prot_carne = .09
+grasime_carne = .03
+
+orez_p = .07
+orez_g = .008
+
+grame_prot = mancare * prot_carne
+grame_grasime = mancare * grasime_carne
