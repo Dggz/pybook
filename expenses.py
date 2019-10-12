077943b14b0bef4572a7d2d346464d43fbde2004
Dggz <cristianbaciu95@gmail.com>
2019-10-12 17:05:20 +0300

    cleaned expenses, shows more data

--- expenses.py
+++ expenses.py
@@ -14,15 +14,15 @@ start_money = int(input("How much money do you have?: "))  # 2000
 print()
 
 [print(dom, amount) for dom, amount in expenses.items()]
-
-left = start_money - sum(expenses.values())
+expense_amount = sum(expenses.values())
+left = start_money - expense_amount
 
 today = datetime.now()
 final_date = final = datetime(2019, datetime.now().month + 1, 6)
 remaining_days = (final_date - today).days + 1
-print(remaining_days)
 
-print("\nMoney left:", int(left))
+print("\nExpenses:", int(expense_amount))
+print("Money left:", int(left))
 print("Daily budget:", int(left / remaining_days))
 print(
     "Daily budget to save up 20% ({}):".format(int(.2 * left)),
